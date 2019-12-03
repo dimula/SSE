1e6a2275f767b0eb0522fca9f7ea4249d9270d28
dimula <dimula@users.noreply.github.com>
2019-12-03 15:18:05 -0800

    Start
    SSE with every second heartbeat is broken and lead to reconnection

---
+++ sse/SSE/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using ServiceStack;
+
+namespace SSE
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public class AppHost : AppHostBase
+        {
+            public AppHost() : base("Hello Web Services", typeof(HelloService).Assembly) { }
+
+            public override void Configure(Funq.Container container)
+            {
+                Plugins.Add(new ServerEventsFeature()
+                {
+                    LimitToAuthenticatedUsers = false,
+                    NotifyChannelOfSubscriptions = false,
+                    OnConnect = (arg1, arg2) =>{},
+                    OnSubscribe = obj =>{},
+                    OnUnsubscribe = obj =>{},
+                });
+            }
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+    }
+}
---
+++ sse/SSE/HelloService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using ServiceStack.Messaging;
+
+namespace SSE
+{
+
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello
+    {
+        public string Name { get; set; }
+    }
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+    [Route("/send")]
+    public class Message
+    {
+        public string Text { get; set; }
+    }
+    public class MessageResponse
+    {
+        public string Result { get; set; }
+    }
+    public class HelloService : Service
+    {
+        public object Get(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, " + request.Name };
+        }
+
+        public object Post(Message message)
+        {
+            return new MessageResponse(){Result = "Ok"};
+        }
+    }
+}
